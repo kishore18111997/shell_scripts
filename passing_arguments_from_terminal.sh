c47c7ef83f6f4fec250291d4ea6b3a2b522928e5
kishore <kishorenate@gmail.com>
2023-09-06 14:09:45 +0530

    script to pass a argument from the terminal while executing the script

---
+++ passing_arguments_from_terminal.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# after typing the execution command of this script type any arguments and that will be displayed on the terminal
+
+echo $1 $2 $3		#this is first way of displaying arguments
+
+echo $#		#to calculate the number of arguments passed from terminal use $#
