8d161672d49a034a3a6f3841a965cf8f596ad530
kishore <kishorenate@gmail.com>
2023-09-08 13:23:07 +0530

    arithmetic calculations of float values, % not working

--- float.sh
+++ float.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-value1=3.5
-value2=2.25
+read -p "enter the first number(you can also use decimal value): " value1
+read -p "enter the second number(you can also use decimal value): " value2
 
 # Use awk to perform addition
-result=$(awk "BEGIN {print $value1 + $value2}")
+sum=$(awk "BEGIN {print $value1 + $value2}")
+diff=$(awk "BEGIN {print $value1 - $value2}")
+mul=$(awk "BEGIN {print $value1 * $value2}")
+div=$(awk "BEGIN {print $value1 / $value2}")
+percentile=(awk "BEGIN {print $value1 % $value2}")
 
-echo "The sum is: $result"
+echo "The sum is: $sum"
+echo "the difference is: $diff"
+echo "the product is: $mul"
+echo "the divided value: $div"
+echo "percentile value: $percentile"
